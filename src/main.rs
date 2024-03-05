51dd4184e59b83c50dff31d6abdb11011135f803
Luky Drum <lukbubi12@gmail.com>
2024-03-05 18:27:08 +0100

    Example UI

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
 mod file_manager;
+mod ui;
 
-use std::path::PathBuf;
+use crate::ui::ui;
 
-use file_manager::Manager;
+use std::io::{self, stdout};
 
-use crate::file_manager::Entry;
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{prelude::*, widgets::*};
 
-fn main() {
-    let mut manager: Manager = Manager::new();
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
-    println!("Before:");
-    for entry in &manager.current_directory_entries {
-        println!("{}", entry.filename());
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        
+        should_quit = handle_events()?;
     }
+
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+    Ok(())
+}
+
+fn handle_events() -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    Ok(false)
 }
--- src/ui.rs
+++ src/ui.rs
@@ -6,30 +6,24 @@ pub fn ui(frame: &mut Frame) {
         [
             Constraint::Length(1),
             Constraint::Min(0),
-            Constraint::Length(1),
         ],
     )
     .split(frame.size());
+
     frame.render_widget(
-        Block::new().borders(Borders::TOP).title("Title Bar"),
+        Block::new().borders(Borders::TOP).title(" Rusty Files "),
         main_layout[0],
     );
-    frame.render_widget(
-        Block::new().borders(Borders::TOP).title("Status Bar"),
-        main_layout[2],
-    );
 
-    let inner_layout = Layout::new(
-        Direction::Horizontal,
-        [Constraint::Percentage(50), Constraint::Percentage(50)],
-    )
-    .split(main_layout[1]);
-    frame.render_widget(
-        Block::default().borders(Borders::ALL).title("Left"),
-        inner_layout[0],
-    );
-    frame.render_widget(
-        Block::default().borders(Borders::ALL).title("Right"),
-        inner_layout[1],
-    );
+    let mut state = ListState::default().with_selected(Some(1));
+
+    let items = ["Item 1", "Item 2", "Item 3"];
+    let list = List::new(items)
+        .block(Block::default().title(" Current directory ").borders(Borders::ALL))
+        .style(Style::default())
+        .highlight_style(Style::default().add_modifier(Modifier::BOLD))
+        .repeat_highlight_symbol(true)
+        .direction(ListDirection::TopToBottom);
+
+    frame.render_stateful_widget(list, main_layout[1], &mut state);
 }
